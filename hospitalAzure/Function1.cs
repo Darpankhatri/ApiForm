10f314b7801803f84efc38046f47539d3fecc56d
Darpan Kumar <darpankhatri817@gmail.com>
2022-12-05 12:34:03 +0500

    azure function

---
+++ hospitalAzure/Function1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace hospitalAzure
+{
+    public static class Function1
+    {
+        [FunctionName("hospitalAzure")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "Welcome To Hospital Medicoz Azure"
+                : $"Hello, {name}. Welcome To Hospital Medicoz Azure";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
